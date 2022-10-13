7693d5fa759ab7f500d70d22af861623f8076c21
Asilbek2002 <school2924@gmail.com>
2022-10-13 17:04:48 +0500

    uptade

--- src/movies/index.js
+++
@@ -1,6 +0,0 @@
-import store, { login, logout, getTotal, getSubTotal } from "./store";
-const subtotal = getSubTotal(store.getState());
-const total = getTotal(store.getState());
-
-console.log("subtotal = ", subtotal);
-console.log("total = ", total);
